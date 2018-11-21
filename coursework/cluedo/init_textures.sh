9c4d2e7b0e87dd5e5f3b8f82a7b5a2aeba8365f5
Jacob Holland <sc15j3h@leeds.ac.uk>
2018-11-21 11:53:58 +0000

    Automated map load process and texture loading

---
+++ coursework/cluedo/init_textures.sh
@@ -0,0 +1,2 @@
+cp -r cluedo_character/ $HOME/.gazebo/models/ &
+cp -r cluedo_weapon/ $HOME/.gazebo/models/
---
+++ coursework/launch_map.sh
@@ -0,0 +1,2 @@
+export TURTLEBOT_GAZEBO_WORLD_FILE=$(pwd)/src/lab5.world
+roslaunch turtlebot_gazebo turtlebot_world.launch
