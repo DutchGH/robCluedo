b591a92352d3b755b06d99240ab7124b8360363b
Emily <sc15an@leeds.ac.uk>
2018-12-16 19:41:22 +0000

    Autonomous navigation using laserscan updated

--- coursework/src/follow_wall.py
+++ coursework/src/follow_wall.py
@@ -7,6 +7,7 @@ import math
 from math import radians
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Float32
 
 class FollowWall:
     def __init__(self):
@@ -21,55 +22,95 @@ class FollowWall:
         self.laserscan = None
 
         self.spinSpeed = 0.1
-        self.wallDist = 0.8
-        self.angle = 0.9
+        self.wallDist = 0.9
+        self.angle = 1.57284
+        self.centre = Float32()
+        self.left = Float32()
+        self.right = Float32()
+        self.left_last = Float32()
+        self.right_last = Float32()
 
-    def laserscan_callback(self, laser_msg):
-        self.laserscan = laser_msg
+    def laserscan_callback(self, laser_data):
+        self.centre.data= laser_data.ranges[320]
+        rospy.loginfo("centre %f", laser_data.ranges[320])
+        self.left.data= laser_data.ranges[639]
+        rospy.loginfo("left %f", laser_data.ranges[639])
+        self.right.data= laser_data.ranges[0]
+        rospy.loginfo("right %f", laser_data.ranges[0])
+        self.laserscan = laser_data
 
     def start(self):
         rospy.loginfo('Start Following Wall')
 
         while not rospy.is_shutdown():
             if (self.laserscan):
-                minLaserValue = min(min(self.laserscan.ranges[170:190], self.laserscan.ranges[150:170]))
-                self.velocity.linear.x = 0.2
-                self.velocity.angular.z = 0.0
-                self.findWall(minLaserValue)
-
-            self.velocityPublish.publish(self.velocity)
+                # minLaserValue = min(min(self.laserscan.ranges[170:190], self.laserscan.ranges[150:170]))
+                # self.findWall(minLaserValue)
+                self.followWall()
 
             self.rate.sleep()
             # rospy.spin()
 
-    def findWall(self, minLaserValue):
-        rospy.loginfo('Find Wall')
-        print(minLaserValue)
-        print(' -- ')
-        print(self.wallDist)
-        if(minLaserValue < self.wallDist):
-            self.rotate(self.angle, self.spinSpeed)
-        else:
-            print('minLaserValue > wallDist')
+    def findClosestWall():
+        print('Closest Wall')
+        # go to it and followWall()
+        # if get too far away from wall findClosestWall()
 
-    def rotate(self, angle, speed):
-        self.velocity.linear.x = 0.0
-        self.velocity.angular.z = radians(45)
+    def followWall(self):
+        if (self.laserscan):
+            print('START')
+            if (self.centre.data > 1):
+                # move forward
+                print('centre > 1')
+                self.moveForward()
+                if (self.left.data < 0.8):
+                    # close to left object, turn right
+                    print('left < 0.8')
+                    self.rotateRight()
+                elif (self.right.data < 0.8):
+                    # close to right object, turn left
+                    print('right < 0.8')
+                    self.rotateLeft()
+            else :
+                print(' else ')
+                if (self.left.data > 0.8):
+                    print('left > 0.8')
+                    self.rotateRight()
+                elif (self.right.data > 0.8):
+                    print('right > 0.8')
+                    self.rotateLeft()
+                elif (self.left.data > self.right.data):
+                    print('left > right')
+                    self.rotateRight()
+                else :
+                    print ('right > left')
+                    self.rotateLeft()
 
-        cur_angle = 0
-        cur_time = rospy.Time.now().to_sec()
-
-        self.velocityPublish.publish(self.velocity)
-        # self.stop()
+        self.rate.sleep()
 
     def stop(self):
         self.velocity.linear.x = 0.0
         self.velocity.angular.z = 0
         self.velocityPublish.publish(self.velocity)
 
+    def moveForward(self):
+        self.velocity.linear.x = 0.2
+        self.velocity.angular.z = 0
+        self.velocityPublish.publish(self.velocity)
+
+    def rotateRight(self):
+        self.velocity.linear.x = 0
+        self.velocity.angular.z = -self.angle
+        self.velocityPublish.publish(self.velocity)
+
+    def rotateLeft(self):
+        self.velocity.linear.x = 0
+        self.velocity.angular.z = self.angle
+        self.velocityPublish.publish(self.velocity)
+
 if __name__ == '__main__':
     try:
         wallFollower = FollowWall()
-        wallFollower.start();
+        wallFollower.start()
     except rospy.ROSInterruptException:
 		pass
