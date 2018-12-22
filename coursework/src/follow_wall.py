4c903688dffe5343f6b9bb50336d7fbba616947a
Emily <sc15an@leeds.ac.uk>
2018-12-22 13:57:27 +0000

    Brought file to same version that is on the NavigationJLB branch

--- coursework/src/follow_wall.py
+++ coursework/src/follow_wall.py
@@ -11,24 +11,25 @@ from std_msgs.msg import Float32
 class FollowWall:
     def __init__(self):
         # subscribe to laserscan topic
-        rospy.Subscriber('/scan', LaserScan, self.laserscan_callback)
         self.velocityPublish = rospy.Publisher('/mobile_base/commands/velocity', Twist, queue_size = 10)
 
         self.velocity = Twist()
-        self.laserscan = None
 
         self.wall_dist = 0.13
-        self.max_speed = 0.3
-        self.direction = 1
-        self.p = 1
-        self.d = 0.5
-        self.angle = 1
+        self.speed = 0.2
+        self.d = 0.4
 
         self.e = 0
-        # Angle, at which was measured the shortest distance
+        # Angle, at which the shortest distance was measured
         self.angle_minDist = 0
         self.dist_front = 0
         self.diff_e = 0
+
+    def start(self):
+        self.laserscanSubs = rospy.Subscriber('/scan', LaserScan, self.laserscan_callback)
+
+    def stop(self):
+        self.laserscanSubs.unregister()
 
     def laserscan_callback(self, scan_data):
         # append non nan values to a new list
@@ -38,18 +39,17 @@ class FollowWall:
                 laserValues.append(scan_data.ranges[i])
 
         size = len(laserValues)
-        min_index = size * (self.direction + 1)/4
-        max_index = size * (self.direction + 3)/4
+        min_index = int(size/2)
+        max_index = size
 
         # find minimum
         for i in range(min_index, max_index):
-            if (laserValues[i] < laserValues[min_index] and laserValues[i] > 0.01):
+            if (laserValues[i] < laserValues[min_index] and laserValues[i] > 0.2):
                 min_index = i
 
-        # Calculation of angles from indexes and storing data to class variables
-        self.angle_minDist = (min_index - size/2) * scan_data.angle_increment
+        self.angle_minDist = (min_index - (size/2)) * scan_data.angle_increment
         dist_min = laserValues[min_index]
-        self.dist_front = laserValues[size/2]
+        self.dist_front = laserValues[int(size/2)]
         self.diff_e = (dist_min - self.wall_dist) - self.e
         self.e = dist_min - self.wall_dist
 
@@ -57,26 +57,38 @@ class FollowWall:
 
     def movement(self):
         # PD controller
-        self.velocity.angular.z = self.direction*(self.p*self.e+self.d*self.diff_e) + self.angle*(self.angle_minDist-math.pi*self.direction/2)
+        self.velocity.angular.z = (self.e + self.d * self.diff_e) + (self.angle_minDist - math.pi * 0.5)
         if (self.dist_front < self.wall_dist):
-            print('dist_front < wall_dist')
             self.velocity.linear.x = 0
         elif (self.dist_front < self.wall_dist * 2):
-            print('dist_front < wall_dist * 2')
-            self.velocity.linear.x = 0.5 * self.max_speed
+            self.velocity.linear.x = 0.5 * self.speed
         elif (abs(self.angle_minDist) > 1.75):
-            print('angle_minDist > 1.75')
-            self.velocity.linear.x = 0.4 * self.max_speed
+            self.velocity.linear.x = 0.4 * self.speed
         else:
-            print('else')
-            self.velocity.linear.x = self.max_speed
+            self.velocity.linear.x = self.speed
 
+        self.velocityPublish.publish(self.velocity)
+
+    def rotate(self):
+        end_angle = radians(360)
+        self.velocity.linear.x = 0
+        self.velocity.angular.z = 0.3
+        t0 = rospy.Time.now().to_sec()
+        current_angle = 0
+
+        while current_angle <= end_angle:
+            self.velocityPublish.publish(self.velocity)
+            t1 = rospy.Time.now().to_sec()
+            current_angle = 0.5*(t1-t0)
+
+        self.velocity.angular.z = 0
         self.velocityPublish.publish(self.velocity)
 
 if __name__ == '__main__':
     try:
         rospy.init_node('wallFollower', anonymous=True)
         wallFollower = FollowWall()
+        wallFollower.start()
         # don't exit until stopped
         rospy.spin()
     except rospy.ROSInterruptException:
